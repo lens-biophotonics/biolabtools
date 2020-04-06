4c8ae303c9f7ae813aca2679fe5683436aaa2aca
Giacomo Mazzamuto <mazzamuto@lens.unifi.it>
2020-04-06 15:37:52 +0200

    dualspim_reslice: fix shear factor sign

--- biolabtools/dualspim_reslice.py
+++ biolabtools/dualspim_reslice.py
@@ -72,7 +72,7 @@ def main():
     T = [0, 0, 0]
     R = np.eye(3)
     Z = [1, 1, r]  # make voxel isotropic
-    S = [0, r * math.tan(theta), 0]
+    S = [0, r * abs(math.tan(theta)), 0]
 
     MO = affines.compose(T, R, Z, S)
 
